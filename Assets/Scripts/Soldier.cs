ba7e4f09557c03f6178760d997e31902f33e95ff
DESKTOP-EODNBCD\kkb96 <kkb9655@gmail.com>
2022-06-11 17:30:56 +0900

    카메라 방향으로 이동, 총이랑 머지

--- Assets/Scripts/Soldier.cs
+++ Assets/Scripts/Soldier.cs
@@ -88,7 +88,7 @@ public class Soldier : MonoBehaviour
 
     private void Run()
     {
-        heading = mainCamera.transform.localRotation * Vector3.forward;
+        heading = transform.localRotation * Vector3.forward;
         heading.y = 0;
         heading = heading.normalized;
         
